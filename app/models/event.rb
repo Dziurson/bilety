8505481f36197401cba5d9d7b3bcaac2634dbe47
Jakub Dziwura <jakub.dziwura@gmail.com>
2019-01-26 18:58:34 +0100

    Rozszerzenie kontrolerów i dodanie bootstrapa

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ApplicationRecord
+    has_many :tickets
+
     validates :artist, presence: true, length: { minimum: 6 }
+
+    def event_date_not_from_past 
+        if event_date < Date.today          
+            errors.add('Data wydarzenia', 'nie może być z przeszłości')     
+        end   
+    end 
+
+    
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,6 @@ class Ticket < ApplicationRecord
     validates :name, presence: true, length: { minimum: 6 } 
     validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :price, presence: true 
+
+    belongs_to :event  
 end
