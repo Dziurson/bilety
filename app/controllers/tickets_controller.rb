151ae03660ad480e09d3e7fa82efd9064114f71a
Jakub Dziwura <jakub.dziwura@gmail.com>
2019-01-27 14:04:49 +0100

    Obsługa relacji ticket <-> user

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -27,7 +27,9 @@ class TicketsController < ApplicationController
   # POST /tickets.json
   def create
     @ticket = Ticket.new(ticket_params)
-
+    @ticket.event = Event.find(ticket_params[:event_id]);
+    @ticket.user = User.find(ticket_params[:user_id]);
+    
     respond_to do |format|
       if @ticket.save
         format.html { redirect_to @ticket, notice: 'Ticket was successfully created.' }
@@ -76,6 +78,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.require(:ticket).permit(:name, :seat_id_seq, :address, :price, :email_address, :phone)
+      params.require(:ticket).permit(:name, :seat_id_seq, :address, :price, :email_address, :phone, :event_id, :user_id)
     end    
 end
